37d228c6de31004f8e330dd7fee5a3b8c84192e9
Fran Sabino <francieine.sabino@gmail.com>
2023-12-21 15:11:22 -0300

    Criacao dos botoes adionar tarefas e limpar

---
+++ src/classes/AppFrame.java
@@ -0,0 +1,30 @@
+package classes;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+public class AppFrame extends JFrame{
+
+	//atributes
+	
+	private TitleBar title;
+	private List list;
+	private ButtonPanel btnPanel;
+	
+	//constructor
+	
+	AppFrame(){
+		this.setSize(400,700);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+		
+		title = new TitleBar();
+		list = new List();
+		btnPanel = new ButtonPanel();
+		this.add(title, BorderLayout.NORTH);
+		this.add(btnPanel, BorderLayout.SOUTH);
+	}
+	
+	
+}
